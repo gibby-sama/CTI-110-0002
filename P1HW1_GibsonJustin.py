ca585a1fecbab02e216b17f1f6ff0fe0ac962b93
gibby-sama <143131869+gibby-sama@users.noreply.github.com>
2023-09-12 11:24:21 -0400

    Add files via upload

---
+++ P1HW1_GibsonJustin.py
@@ -0,0 +1,12 @@
+user_num = int(input('Enter integer:\n'))
+squared = user_num * user_num
+cubed = user_num * user_num * user_num
+
+print("You entered:", user_num)
+print(user_num, "squared is", squared)
+print("And", user_num, "cubed is", cubed, "!!")
+user_num2 = int(input('Enter another integer:\n'))
+sum = user_num + user_num2
+product = user_num * user_num2
+print(user_num, "+", user_num2, "is", sum)
+print(user_num, "*", user_num2, "is", product)
